4ca02a8d1a5291df9415f44493bbb9c049ac0eb9
andisltn48 <61143381+andisltn48@users.noreply.github.com>
2022-10-04 18:40:51 +0800

    update transaction service

--- src/transactions/transactions.module.ts
+++ src/transactions/transactions.module.ts
@@ -3,10 +3,9 @@ import { TransactionsService } from './transactions.service';
 import { TransactionsController } from './transactions.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Transaction,TransactionSchema  } from './schemas/transaction.schema';
-import { TransactionListGateway } from './transaction-list.gateway';
 
 @Module({
-  providers: [TransactionsService, TransactionListGateway],
+  providers: [TransactionsService],
   controllers: [TransactionsController],
   imports: [
     MongooseModule.forFeature([{
